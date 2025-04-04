59a75240e63ca41efa3211d08d2066ac54edee7f
Oleksandr Romaniuk <oleksandr.romaniuk@protonmail.com>
2025-04-04 12:06:16 +0000

    move routers and routes to sep file

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
 	"github.com/olksndrdevhub/go-api-starter-kit/db"
-	"github.com/olksndrdevhub/go-api-starter-kit/handlers"
 	"github.com/olksndrdevhub/go-api-starter-kit/middleware"
 	"log"
 	"net/http"
@@ -16,38 +14,9 @@ func main() {
 	}
 	defer db.CloseDBConn()
 
-	// base router
 	baseRouter := http.NewServeMux()
 
-	// health check router
-	statusRouter := http.NewServeMux()
-	statusRouter.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		fmt.Fprintf(w, `{"status": "ok"}`)
-	})
-
-	// safe apiRouter (no auth)
-	apiRouter := http.NewServeMux()
-	apiRouter.HandleFunc("POST /register", handlers.Register)
-	apiRouter.HandleFunc("POST /login", handlers.Login)
-
-	// unsafe API router (jwt auth)
-	apiJwtRouter := http.NewServeMux()
-	apiJwtRouter.HandleFunc("/me", handlers.Profile)
-	apiJwtRouter.HandleFunc("POST /me/change-password", handlers.ChangePassword)
-
-	// api versioning
-	apiV1Router := http.NewServeMux()
-	apiV1Router.Handle("/v1/auth/", http.StripPrefix("/v1/auth", apiRouter))
-	apiV1Router.Handle("/v1/", middleware.JWTMiddleware(http.StripPrefix("/v1", apiJwtRouter)))
-
-	// admin router (TODO:)
-	adminRouter := http.NewServeMux()
-	adminRouter.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {})
-
-	baseRouter.Handle("/api/", http.StripPrefix("/api", apiV1Router))
-	baseRouter.Handle("/admin/", http.StripPrefix("/admin", adminRouter))
-	baseRouter.Handle("/status/", http.StripPrefix("/status", statusRouter))
+	baseRouter = loadRouters(baseRouter)
 
 	// Create a middleware chain
 	middlewareStuck := middleware.CreateStuck(
---
+++ router.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"github.com/olksndrdevhub/go-api-starter-kit/handlers"
+	"github.com/olksndrdevhub/go-api-starter-kit/middleware"
+	"net/http"
+)
+
+func loadRouters(baseRouter *http.ServeMux) *http.ServeMux {
+	// health check router
+	statusRouter := http.NewServeMux()
+	statusRouter.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprintf(w, `{"status": "ok"}`)
+	})
+
+	// safe apiRouter (no auth)
+	apiRouter := http.NewServeMux()
+	apiRouter.HandleFunc("POST /register", handlers.Register)
+	apiRouter.HandleFunc("POST /login", handlers.Login)
+
+	// unsafe API router (jwt auth)
+	apiJwtRouter := http.NewServeMux()
+	apiJwtRouter.HandleFunc("/me", handlers.Profile)
+	apiJwtRouter.HandleFunc("POST /me/change-password", handlers.ChangePassword)
+
+	// api versioning
+	apiV1Router := http.NewServeMux()
+	apiV1Router.Handle("/v1/auth/", http.StripPrefix("/v1/auth", apiRouter))
+	apiV1Router.Handle("/v1/", middleware.JWTMiddleware(http.StripPrefix("/v1", apiJwtRouter)))
+
+	// admin router (TODO:)
+	adminRouter := http.NewServeMux()
+	adminRouter.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {})
+
+	baseRouter.Handle("/api/", http.StripPrefix("/api", apiV1Router))
+	baseRouter.Handle("/admin/", http.StripPrefix("/admin", adminRouter))
+	baseRouter.Handle("/status/", http.StripPrefix("/status", statusRouter))
+
+	return baseRouter
+}
